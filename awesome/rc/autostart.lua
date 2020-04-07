ab0a33641f9a876c7e244c34a848fb2043c54257
Zaur Nasibov <zaur@znasibov.info>
2020-04-07 08:54:14 +0300

    Awesome: Run thunderbird only on Home machine

--- awesome/rc/autostart.lua
+++ awesome/rc/autostart.lua
@@ -2,8 +2,13 @@ loadrc("util", "zawesome/util")
 
 -- run_once("glipper", nil, ".*glipper")
 run_once("xscreensaver", "-no-splash", ".*xscreensaver.*", nil)
-run_once("vivaldi-snapshot", nil, ".*vivaldi-bin.*")
+run_once("vivaldi", nil, ".*vivaldi-bin.*")
 run_once("emacs")
 run_once("Telegram", nil, ".*Telegram.*")
-run_once("thunderbird")
-run_once("dropbox start")
+
+hostname = io.popen("uname -n"):read()
+
+if hostname == "z" then
+   run_once("thunderbird")
+   run_once("dropbox start")
+end
