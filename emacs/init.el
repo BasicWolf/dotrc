eb66fabf07f077836b288a66d443cf4d702cd3cb
Zaur Nasibov <zaur@znasibov.info>
2021-09-06 05:27:16 +0300

    Emacs: More new-style configuration

--- emacs/init.el
+++ emacs/init.el
@@ -3,7 +3,7 @@
 ;; Good luck to me!
 ;; Inspired by
 ;; * https://github.com/a13/emacs.d
-
+;; * https://github.com/howardabrams/dot-files
 
 
 ;; GLOBALS
@@ -11,12 +11,25 @@
 (defvar var-dir (concat init-dir "var/"))
 
 (defun main()
+  (message "Main started")
+
   (dotemacs/repositories)
-  (dotemacs/bootstrap)
-  (dotemacs/ui)
-  )
+  (dotemacs/package-management)
+  (dotemacs/ui-ux)
+  (dotemacs/ux/highlight)
+  (dotemacs/external-behaviour)
+  (dotemacs/minibuffer)
+  (dotemacs/search)
+  (dotemacs/dired)
+
+  ;; Major editor modes
+  (dotemacs/elisp)
+  (dotemacs/restructured-text)
+  t)
+
 
 (defun dotemacs/repositories ()
+  (message "dotemacs/repositories")
     ;; By default, Emacs knows about GNU ELPA only, add some more repositories.
   (require 'package)
   (customize-set-variable 'package-archives
@@ -28,14 +41,29 @@
   (package-initialize)
   t)
 
-(defun dotemacs/bootstrap ()
-  ;; Use-package installation and setup
+
+(defun dotemacs/package-management ()
+  (message "dotemacs/package-management")
+  ;; Use-package and Quelpa installation and setup
   ;; It can’t install itself so we have to bootstrap it
   (unless (package-installed-p 'use-package)
     (package-refresh-contents)
-    (package-install 'use-package))
+    (package-install 'use-package)
+
+    ;; Quelpa “is a tool to compile and install Emacs Lisp packages locally
+    ;  from local or remote source code”. No more manual package installation
+    ;; This is used for example to install dired+ directly from Github.
+    (package-install 'quelpa)
+    (package-install 'quelpa-use-package))
 
   (eval-when-compile
+    ;; quelpa setup before use-package to avoid
+    ;; "Failed to parse package XXX: use-package: Unrecognized keyword: :quelpa"
+    (require 'quelpa)
+    (require 'quelpa-use-package)
+    (setq quelpa-update-melpa-p nil) ; "Don't update the MELPA git repo."
+    (setq quelpa-use-package-inhibit-loading-quelpa t)
+
     (require 'use-package))
 
   (put 'use-package 'lisp-indent-function 1)
@@ -69,16 +97,8 @@
   t)
 
 
-(defun dotemacs/ui ()
-  (use-package emacs
-    :load-path "secrets"
-    :init
-    (fset 'yes-or-no-p 'y-or-n-p)  ;; replace yes/no questions with y/n
-    (toggle-indicate-empty-lines)  ;; show the empty lines at the end (bottom) of the buffer
-    (blink-cursor-mode -1)         ;; the blinking cursor is pretty annoying, so disable it.
-    ;:custom
-    ;(default-frame-alist '(tool-bar-lines 0))       ;; hide tool bar
-    )
+(defun dotemacs/external-behaviour ()
+  (message "dotemacs/external-behaviour")
 
   (use-package files
     :hook
@@ -94,6 +114,35 @@
     (backup-directory-alist
      `((".*" . ,(locate-user-emacs-file "var/backups"))))
     )
+  )
+
+
+(defun dotemacs/ui-ux ()
+  (message "dotemacs/ui-ux")
+
+  (use-package emacs
+    :init
+    (fset 'yes-or-no-p 'y-or-n-p)    ;; replace yes/no questions with y/n
+    (toggle-indicate-empty-lines)    ;; show the empty lines at the end (bottom) of the buffer
+    (blink-cursor-mode 0)            ;; the blinking cursor is pretty annoying, so disable it.
+    (tool-bar-mode 0)                ;; disable toolbar
+    (global-prettify-symbols-mode 1) ;; render "lambda" as λ
+    :custom
+    (show-trailing-whitespace t)     ;; show trailing whitespace in all modes
+    (indent-tabs-mode nil)           ;; spaces instead of tabs
+    (tab-width 4)                    ;; default tab width
+    (frame-title-format
+     '("Emacs - " buffer-file-name "%f" ("%b"))) ;; change emacs window title
+    (column-number-mode 1)           ;; show the column number
+    (transient-mark-mode 0)          ;; don't show region selection
+    (global-display-fill-column-indicator-mode 1)  ;; show vertical column indicator ...
+    (display-fill-column-indicator-column 80)      ;; ... on 80th column position
+    (global-display-line-numbers-mode 1)           ;; show line numbers everywhere
+
+    :bind
+    ;; kill current buffer, without prompting
+    (("\C-xk" . (lambda () (interactive) (kill-buffer (current-buffer)))))
+    )
 
   ;; Enable shift-arrows switching between windows
   (use-package windmove
@@ -104,6 +153,18 @@
   (use-package winner
     :config
     (winner-mode 1))
+
+  (use-package tabbar
+    :ensure
+    :bind
+    (("M-<up>" . 'tabbar-backward-tab)
+     ("M-<down>" . 'tabbar-forward-tab)
+     ("M-S-<up>" . 'tabbar-backward-group)
+     ("M-S-<down>" . 'tabbar-forward-group))
+    :custom
+    (tabbar-mode t)               ;; enable tabbar mode
+    (tabbar-use-images nil)       ;; speed up thing displaying characters instead of images in left-top corner
+    (tabbar-cycle-scope 'tabs))   ;; cycle through tabs of current group
 
   (use-package desktop
     :config
@@ -130,22 +191,153 @@
       )
     )
 
-
   (use-package solarized-theme
     :ensure
     :config
     (load-theme 'solarized-light t)
     :custom
     (solarized-distinct-fringe-background t)
+
+    ;; Determines how to draw underlined text.
+    ;; nil - draw it at the baseline level of the font;
+    ;; t -   draw the underline at the same height as the font’s descent line.
+    (x-underline-at-descent-line t)
+    ;; The brightness of the background hint to font renderer.
+    (frame-background-mode 'light))
+  t)
+
+
+(defun dotemacs/minibuffer ()
+  (message "dotemacs/minibuffer")
+  ;; Ivy is an interactive interface for completion in Emacs.
+  ;; Ivy is for quick and easy selection from a list. When Emacs
+  ;; prompts for a string from a list of several possible choices,
+  ;; Ivy springs into action to assist in narrowing and picking
+  ;; the right string from a vast number of choices.
+  ;; Ivy strives for minimalism, simplicity, customizability and discoverability.
+  ;; More at https://oremacs.com/swiper/
+  (use-package ivy
+    :ensure t
+    :custom
+    (ivy-count-format "%d/%d ")   ;; ivy completions counter format, e.g. 2/9
+    (ivy-use-selectable-prompt t) ;; "Make the prompt line selectable"
+    :config
+    (ivy-mode t))
+
+  ;; TODO
+  (use-package ivy-xref
+    :ensure t
+    :custom
+    (xref-show-definitions-function #'ivy-xref-show-defs))
+
+  t)
+
+
+(defun dotemacs/search ()
+  (message "dotemacs/search")
+  ;; Superfast Silver Searcher tool
+  (use-package ag
+    :ensure t
+    :defer t
+    :ensure-system-package (ag . silversearcher-ag)
+    :custom
+    (ag-highlight-search t)) ;; Highlight the current search term
+
+  ;; Dumb Jump is an Emacs "jump to definition" package with support for 50+
+  ;; programming languages that favors "just working".
+  ;; This means minimal -- and ideally zero -- configuration with absolutely
+  ;; no stored indexes (TAGS) or persistent background processes
+  (use-package dumb-jump
+    :ensure t
+    :defer t
+    :custom
+    (dumb-jump-selector 'ivy)
+    (dumb-jump-prefer-searcher 'ag))
+  t)
+
+
+(defun dotemacs/ux/highlight ()
+  (message "dotemacs/ux/highlight")
+
+  (use-package paren
+    :config
+    (show-paren-mode t))
+
+  ;; Render parens, brackets, and braces according to their depth.
+  ;; Each successive level is highlighted a different color.
+  (use-package rainbow-delimiters
+    :ensure t
+    :hook
+    (prog-mode . rainbow-delimiters-mode))
+
+  ;; Highlight current cursor line in prog modes
+  (use-package hl-line
+    :hook
+    (prog-mode . hl-line-mode))
+
+  ;; Render numeric literals in source code via configured font
+  (use-package highlight-numbers
+    :ensure t
+    :hook
+    (prog-mode . highlight-numbers-mode))
+
+  ;; Render TODO in comments via Italic font
+  (use-package hl-todo
+    :ensure t
+    :custom-face
+    (hl-todo ((t (:inherit hl-todo :italic t))))
+    :hook ((prog-mode . hl-todo-mode)
+           (yaml-mode . hl-todo-mode)))
+  t)
+
+
+(defun dotemacs/dired ()
+  (use-package dired+
+    :quelpa
+    (dired+ :fetcher github :repo "emacsmirror/dired-plus")
+    :custom
+    (diredp-hide-details-initially-flag nil)
+    (toggle-diredp-find-file-reuse-dir 1)
+    (dired-dwim-target t)
+    (dired-recursive-copies 'top)
+    (dired-recursive-deletes 'top)
+    (diredp-hide-details-initially-flag nil)
+    (dired-listing-switches "-alh")
     )
+
+   t)
+
+
+(defun dotemacs/elisp ()
+  (message "dotemacs/elisp")
+
+  (use-package lisp
+    :init
+    (defconst lisp--prettify-symbols-alist
+      '(("lambda"  . ?λ)                  ; Shrink this
+        ("."       . ?•)))                ; Enlarge this
+
+    :hook
+    (after-save . check-parens)) ;; check unbalanced parenthesis in current buffer
+
+  (use-package eros
+    :ensure t
+    :init
+    (add-hook 'emacs-lisp-mode-hook (lambda () (eros-mode 1))))
+  t)
+
+(defun dotemacs/restructured-text ()
+  (message "dotemacs/restructured-text")
 
   (use-package rst)
 
   t)
 
+(defun dotemacs/markdown ()
+  (use-package markdown-mode :ensure t)
+  t)
 
 (main)
-
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -157,10 +349,10 @@
      ("org" . "https://orgmode.org/elpa/")))
  '(package-enable-at-startup nil)
  '(package-selected-packages
-   '(solarized-theme paradox use-package-ensure-system-package system-packages use-package epc)))
+   '(markdown-mode quelpa-use-package quelpa eros ag ivy-xref ivy tabbar epc use-package-ensure-system-package solarized-theme rainbow-delimiters paradox hl-todo highlight-numbers highlight-escape-sequences gcmh)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(hl-todo ((t (:inherit hl-todo :italic t)))))
